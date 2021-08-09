01fb0a1f7df1fd3bb618f890e36788c588ff56e8
Jens Wiecker <j.wiecker@gmx.de>
2021-08-09 23:56:13 +0200

    add sass and browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
-let mode = "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-if(process.env.NODE_ENV === "production") {
-   mode = "production";
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = "browserslist";
 }
 module.exports = {
     mode: mode,
+    target: target,
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_module/,
-            use: {
-                loader: "babel-loader"
+        rules: [
+            {
+                test: /\.s?css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
-        }]
+        ],
     },
+    plugins: [new MiniCssExtractPlugin()],
     devtool: "source-map",
     devServer: {
         contentBase: "./dist",
+        hot: true,
     }
 }
